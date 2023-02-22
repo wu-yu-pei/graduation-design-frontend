81f91f7fc4b98bf4a34aae90560ce984ce5316f6
wuyupei <495174699@qq.com>
2023-02-22 14:24:38 +0800

    fea:智能物流

--- src/pages/main/ZhiNengWuLui.vue
+++ src/pages/main/ZhiNengWuLui.vue
@@ -2,7 +2,7 @@
   <div class="box">
     <div class="top">
       <div class="top-tip">注意:对话不保留</div>
-      <div class="message">
+      <div class="message" ref="contentEl">
         <div v-for="(item, index) in message">
           <div class="self" v-if="item.from == 'self'">
             <img :src="userInfo.img" alt="" />
@@ -19,7 +19,7 @@
           <div class="outher" v-else>
             <img src="/image/rabbot.png" alt="" />
             <div>
-              <p>智能助理</p>
+              <p>智能助理 {{ item.date }}</p>
               <div class="outher-content" v-html="item.message"></div>
             </div>
           </div>
@@ -28,7 +28,8 @@
     </div>
     <div class="bottom">
       <el-input @enter="send" placeholder="请输入你的问题" v-model="question"></el-input>
-      <el-button type="primary" @click="send">发送</el-button>
+      <el-button v-if="!isLoading" type="primary" @click="send">发送</el-button>
+      <el-button v-else v-loading="isLoading" type="primary" disabled> </el-button>
     </div>
   </div>
 </template>
@@ -37,72 +38,100 @@ meta:
   name: 智能物流
 </route>
 <script setup>
-import { useStorage } from '@vueuse/core';
+import { useStorage, useDateFormat } from '@vueuse/core';
 import { storeToRefs } from 'pinia';
 import useAppStore from '../../store/app';
 import { getInfoByid } from '../../service/home/index';
 
 const appStore = useAppStore();
 const { userInfo } = storeToRefs(appStore);
+let contentEl = ref(null);
 let question = ref('');
+let isLoading = ref(false);
 
-let message = useStorage('message', [{ from: 'outher', message: '你好欢迎使用智能助理: 使用方法:向我发送物流号即可例如：#23', date: 1655260176099 }], sessionStorage);
+let message = useStorage(
+  'message',
+  [{ from: 'outher', message: '你好欢迎使用智能助理: 使用方法:向我发送物流号即可例如：#23', date: useDateFormat(+new Date(), 'YYYY-MM-DD HH:mm:ss') }],
+  sessionStorage
+);
 
 async function send() {
   question.value = question.value.trim();
   if (!question.value) {
-    return alert('请输入问题');
+    return ElMessage({
+      type: 'warning',
+      message: '请输入内容',
+    });
   }
   message.value.push({
     from: 'self',
     message: question.value,
     date: +new Date(),
   });
+  isLoading.value = true;
+  await nextTick();
+  contentEl.value.scrollTop = contentEl.value.scrollHeight;
 
   const tag = question.value.slice(0, 1);
   const id = question.value.slice(1);
-
+  question.value = '';
+  console.log(tag == '#', /^\d*$/.test(id));
   if (tag == '#' && /^\d*$/.test(id)) {
     const res = await getInfoByid(id);
-    console.log(res.data.length);
+    isLoading.value = false;
     if (res.data.length != 0) {
       const { id, start_position, end_position, name, current_position, current_time, status } = res.data[0];
       message.value.push({
         from: 'outher',
         message: `
           物流编号：#${id}<br/>
-
+          <br/>
           状态：${status == 0 ? '已完成' : status == 1 ? '运输中' : '已拦截'}<br/>
+          <br/>
           商品名称：${name}<br/>
+          <br/>
           发货地址：${start_position}<br/>
+          <br/>
           收货地址：${end_position}<br/>
+          <br/>
         `,
-        date: +new Date(),
+        date: useDateFormat(+new Date(), 'YYYY-MM-DD HH:mm:ss'),
       });
+      await nextTick();
+      contentEl.value.scrollTop = contentEl.value.scrollHeight;
     } else {
       message.value.push({
         from: 'outher',
         message: '抱歉，没有该物流信息！',
         date: +new Date(),
       });
+      await nextTick();
+      contentEl.value.scrollTop = contentEl.value.scrollHeight;
     }
   } else {
-    setTimeout(() => {
+    setTimeout(async () => {
+      isLoading.value = false;
       message.value.push({
         from: 'outher',
         message: '抱歉，我只认识物流编码哦！',
-        date: +new Date(),
+        date: useDateFormat(+new Date(), 'YYYY-MM-DD HH:mm:ss'),
       });
-    }, 500);
+      await nextTick();
+      contentEl.value.scrollTop = contentEl.value.scrollHeight;
+    }, 1000);
   }
-  question.value = '';
 }
+
+onMounted(() => {
+  contentEl.value.scrollTop = contentEl.value.scrollHeight;
+});
 </script>
 
 <style scoped lang="less">
 .box {
   height: 100%;
   .top {
+    position: relative;
     height: calc(100% - 60px);
     border: 1px solid #dcdfe6;
     padding: 10px 0;
@@ -118,6 +147,7 @@ async function send() {
       height: calc(100% - 20px);
       padding: 10px;
       overflow-y: scroll;
+      scroll-behavior: smooth;
       .self {
         display: flex;
         flex-direction: row-reverse;
