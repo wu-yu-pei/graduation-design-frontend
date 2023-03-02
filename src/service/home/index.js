0d40706079c0834ef05a1172c708c3251de4e628
wuyupei <495174699@qq.com>
2023-03-02 17:05:09 +0800

    fix:token

--- src/service/home/index.js
+++ src/service/home/index.js
@@ -10,26 +10,11 @@ export function login(data) {
 export function findShop(uid) {
   return request.get({
     url: '/shop',
-    params: {
-      uid,
-    },
   });
 }
 
 export function createShop(data) {
   return request.post({
-    interceptors: {
-      interceptorsRequest(config) {
-        const token = localStorage.getItem('token');
-        if (token) {
-          config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-      },
-      InterceptorsRequestCatch(err) {
-        return err;
-      },
-    },
     url: '/createshop',
     data,
   });
