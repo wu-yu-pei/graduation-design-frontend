95403963f43f5f8cb4a004552e0700835474036e
wuyupei <495174699@qq.com>
2023-03-13 18:53:49 +0800

    fix

--- src/pages/index/Self.vue
+++ src/pages/index/Self.vue
@@ -15,7 +15,7 @@
           <el-input :disabled="currentStatus == 0" v-model="formInline.account" placeholder="Approved by" />
         </el-form-item>
         <el-form-item label="操作:">
-          <el-button v-if="currentStatus == 0" @click="changeCurrentStatus()">修改</el-button>
+          <el-button :disabled="time != 0" v-if="currentStatus == 0" @click="changeCurrentStatus()">{{ time == 0 ? '修改' : `${time}后可修改` }}</el-button>
           <el-button v-if="currentStatus == 1" v-loading="currentStatus == isChanging" @click="sureChange()">确定</el-button>
         </el-form-item>
       </el-form>
@@ -34,6 +34,7 @@ import { changeUserInfo } from '../../service/home/index';
 import useAppStore from '../../store/app';
 let currentStatus = ref(0); // 0:禁止编辑状态 1:编辑状态
 let isChanging = ref(false);
+let time = ref(0);
 const appStore = useAppStore();
 const formInline = reactive({
   uname: appStore.userInfo.uname,
@@ -61,11 +62,23 @@ function sureChange() {
     }
     currentStatus.value = 0;
     isChanging.value = false;
+    setIn(60);
   });
 }
 
 function changeCurrentStatus() {
   currentStatus.value = 1;
+}
+
+function setIn(outTime) {
+  time.value = outTime;
+  let timer = setInterval(() => {
+    time.value--;
+    if (time.value == 0) {
+      clearInterval(timer);
+      time.value = 0;
+    }
+  }, 1000);
 }
 </script>
 
